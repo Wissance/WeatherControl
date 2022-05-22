db69539ff1f59e2c73026c04d075d093efd3778d
Ushakov Michael <um.nix.user@gmail.com>
2022-05-22 18:41:48 +0500

    partially measurements manager done

--- WeatherControl/Wissance.WeatherControl/Managers/MeasurementsManager.cs
+++ WeatherControl/Wissance.WeatherControl/Managers/MeasurementsManager.cs
@@ -6,6 +6,8 @@ using Microsoft.Extensions.Logging;
 using Wissance.WeatherControl.Data;
 using Wissance.WeatherControl.Data.Entity;
 using Wissance.WeatherControl.Dto;
+using Wissance.WeatherControl.WebApi.Factory;
+using Wissance.WebApiToolkit.Dto;
 using Wissance.WebApiToolkit.Managers;
 
 namespace Wissance.WeatherControl.WebApi.Managers
@@ -15,6 +17,31 @@ namespace Wissance.WeatherControl.WebApi.Managers
         public MeasurementsManager(ModelContext modelContext, ILoggerFactory loggerFactory) : base(loggerFactory)
         {
             _modelContext = modelContext;
+        }
+
+        public override async Task<OperationResultDto<IList<MeasurementsDto>>> GetAsync(int page, int size)
+        {
+            return await GetAsync<int>(_modelContext.Measurements, page, size, null, null, MeasurementsFactory.Create);
+        }
+
+        public override async Task<OperationResultDto<MeasurementsDto>> GetByIdAsync(int id)
+        {
+            return await GetAsync(_modelContext.Measurements, id, MeasurementsFactory.Create);
+        }
+
+        public override async Task<OperationResultDto<MeasurementsDto>> CreateAsync(MeasurementsDto data)
+        {
+            return await base.CreateAsync(data);
+        }
+
+        public override async Task<OperationResultDto<MeasurementsDto>> UpdateAsync(int id, MeasurementsDto data)
+        {
+            return await base.UpdateAsync(id, data);
+        }
+
+        public override async Task<OperationResultDto<bool>> DeleteAsync(int id)
+        {
+            return await DeleteAsync(_modelContext, _modelContext.Measurements, id);
         }
 
         private readonly ModelContext _modelContext;
