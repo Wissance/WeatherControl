af24159da9e420e30a545ee6b6e8940dd7ee5999
Ushakov Michael <um.nix.user@gmail.com>
2023-01-14 01:38:31 +0500

    measure unit controller

--- WeatherControl/Wissance.WeatherControl.GraphData/Entity/MeasureUnitEntity.cs
+++ WeatherControl/Wissance.WeatherControl.GraphData/Entity/MeasureUnitEntity.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Wissance.WebApiToolkit.Data.Entity;
 
 namespace Wissance.WeatherControl.GraphData.Entity
 {
-    public class MeasureUnitEntity
+    public class MeasureUnitEntity : IModelIdentifiable<Guid>
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
--- WeatherControl/Wissance.WeatherControl.WebApi.V2/Controllers/MeasureUnitController.cs
+++ WeatherControl/Wissance.WeatherControl.WebApi.V2/Controllers/MeasureUnitController.cs
@@ -1,6 +1,12 @@
-﻿namespace Wissance.WeatherControl.WebApi.V2.Controllers
+﻿using System;
+using Wissance.WebApiToolkit.Controllers;
+using Wissance.WeatherControl.Dto.V2;
+using Wissance.WeatherControl.GraphData.Entity;
+
+namespace Wissance.WeatherControl.WebApi.V2.Controllers
 {
-    public class MeasureUnitController
+    public class MeasureUnitController : BasicReadController<MeasureUnitDto, MeasureUnitEntity, Guid>
     {
+        
     }
 }
