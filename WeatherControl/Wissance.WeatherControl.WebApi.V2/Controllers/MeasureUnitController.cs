3066c77ed7554a4588910e4be8fe71482b22677c
Ushakov Michael <um.nix.user@gmail.com>
2023-01-14 19:53:09 +0500

    edgedb manager linked to proper controller

--- WeatherControl/Wissance.WeatherControl.WebApi.V2/Controllers/MeasureUnitController.cs
+++ WeatherControl/Wissance.WeatherControl.WebApi.V2/Controllers/MeasureUnitController.cs
@@ -2,11 +2,18 @@
 using Wissance.WebApiToolkit.Controllers;
 using Wissance.WeatherControl.Dto.V2;
 using Wissance.WeatherControl.GraphData.Entity;
+using Wissance.WeatherControl.WebApi.V2.Managers;
+using Wissance.WebApiToolkit.Managers;
 
 namespace Wissance.WeatherControl.WebApi.V2.Controllers
 {
     public class MeasureUnitController : BasicReadController<MeasureUnitDto, MeasureUnitEntity, Guid>
     {
+        public MeasureUnitController(EdgeDbManager<MeasureUnitDto, MeasureUnitEntity, Guid> manager)
+        {
+            Manager = manager;
+        }
         
+        //private IModelManager<MeasureUnitDto, MeasureUnitEntity, Guid> _
     }
 }
