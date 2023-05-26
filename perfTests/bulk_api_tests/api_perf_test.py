72de5b295ceec3ab25d7486fe88672cd77b0a1dd
Ushakov Michael <um.nix.user@gmail.com>
2023-05-26 14:39:39 +0500

    added perf tests from external client

---
+++ perfTests/bulk_api_tests/api_perf_test.py
@@ -0,0 +1,11 @@
+import aiohttp
+import asyncio
+
+def main():
+    print("Testing API performance vs Bulk API on WeatherControl application started")
+
+    print("Testing API performance vs Bulk API on WeatherControl application finished")
+
+
+if __name__ == "__main__":
+    main()
