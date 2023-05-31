6d42285d84aa676730ce2765468a6923aa781d22
Ushakov Michael <um.nix.user@gmail.com>
2023-05-31 14:37:39 +0500

    new controller for bulk methods

---
+++ WeatherControl/Wissance.WeatherControl.WebApi.V2/Controllers/MeasurementSeriesController.cs
@@ -0,0 +1,20 @@
+using System;
+using EdgeDB;
+using Wissance.WeatherControl.Dto.V2;
+using Wissance.WeatherControl.GraphData;
+using Wissance.WeatherControl.GraphData.Entity;
+using Wissance.WeatherControl.WebApi.V2.Factories;
+using Wissance.WeatherControl.WebApi.V2.Managers;
+using Wissance.WebApiToolkit.Controllers;
+
+namespace Wissance.WeatherControl.WebApi.V2.Controllers
+{
+    public class MeasurementSeriesController : BasicBulkCrudController<MeasurementDto, MeasurementEntity, Guid>
+    {
+        public MeasurementSeriesController(EdgeDBClient edgeDbClient)
+        {
+            Manager = new EdgeDbManager<MeasurementDto, MeasurementEntity, Guid>(ModelType.Measurement, edgeDbClient,
+                MeasurementFactory.Create, MeasurementFactory.Create);
+        }
+    }
+}
