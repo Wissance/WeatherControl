316316ec1a9d90b714d596e16473175107b1c4a2
Ushakov Michael <um.nix.user@gmail.com>
2023-06-01 17:54:47 +0500

    more tests

--- perfTests/bulk_api_tests/api_perf_test.py
+++ perfTests/bulk_api_tests/api_perf_test.py
@@ -8,13 +8,17 @@ import time
 
 
 async def main():
-    print("Testing API performance vs Bulk API on WeatherControl application started")
-    await run_non_bulk_api(10)
-    await run_bulk_api(10)
-    print("Testing API performance vs Bulk API on WeatherControl application finished")
+    print("Testing API performance vs Bulk API on WeatherControl application with EF started")
+    await run_non_bulk_api_with_ef(10)
+    await run_bulk_api_with_ef(10)
+    print("Testing API performance vs Bulk API on WeatherControl application with EF finished")
+    print("Testing API performance vs Bulk API on WeatherControl application with EdgeDB started")
+    await run_non_bulk_api_with_edgedb(10)
+    await run_bulk_api_with_edgedb(10)
+    print("Testing API performance vs Bulk API on WeatherControl application with EdgeDB finished")
 
 
-async def run_non_bulk_api(num_of_objs: int):
+async def run_non_bulk_api_with_ef(num_of_objs: int):
     measurements_res = "http://127.0.0.1:8058/api/Measurements"
     measurements = list()
     for i in range(0, num_of_objs):
@@ -32,12 +36,6 @@ async def run_non_bulk_api(num_of_objs: int):
         'Accept': 'application/json'
     }
 
-    '''
-        for i in range(0, num_of_objs):
-            resp = requests.post(url=measurements_res, json=measurements[i], headers=headers)
-            if resp.status_code == 201:
-                print("Measurements created")
-    '''
     start = time.time()
     async with aiohttp.ClientSession() as session:
         for i in range(0, num_of_objs):
@@ -46,11 +44,11 @@ async def run_non_bulk_api(num_of_objs: int):
                 if resp.status == 201:
                     created_mes = await resp.json()
     end = time.time()
-    dt = (end - start)
-    print(f"Elapsed time in Non-Bulk REST API with EF is {dt} secs.")
+    dt = end - start
+    print(f"Elapsed time in Non-Bulk REST API with EF on creation of {num_of_objs} items is {dt} secs.")
 
 
-async def run_bulk_api(num_of_objs: int):
+async def run_bulk_api_with_ef(num_of_objs: int):
     measurements_res = "http://127.0.0.1:8058/api/MeasurementsSeries"
     measurements = list()
     for i in range(0, num_of_objs):
@@ -68,12 +66,6 @@ async def run_bulk_api(num_of_objs: int):
         'Accept': 'application/json'
     }
 
-    '''
-        for i in range(0, num_of_objs):
-            resp = requests.post(url=measurements_res, json=measurements[i], headers=headers)
-            if resp.status_code == 201:
-                print("Measurements created")
-    '''
     start = time.time()
     async with aiohttp.ClientSession() as session:
         body = measurements
@@ -81,8 +73,64 @@ async def run_bulk_api(num_of_objs: int):
             if resp.status == 201:
                 created_mes = await resp.json()
     end = time.time()
-    dt = (end - start)
-    print(f"Elapsed time in Bulk API with EF is {dt} secs.")
+    dt = end - start
+    print(f"Elapsed time in Bulk API with EF on creation of {num_of_objs} items is {dt} secs.")
+
+
+async def run_non_bulk_api_with_edgedb(num_of_objs: int):
+    measurements_res = "http://127.0.0.1:8059/api/Measurement"
+    measurements = list()
+    for i in range(0, num_of_objs):
+        measurements.append({
+            "sampleDate": str(datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S")),
+            "value": random.random() * 25,
+            "measureUnitId": "604a0412-ea47-11ed-b9a1-bba352c6e579",
+            "sensorId": "30ac8366-ea4b-11ed-ab17-e38db76a95f0"
+        })
+
+    headers = {
+        'Content-type': 'application/json',
+        'Accept': 'application/json'
+    }
+
+    start = time.time()
+    async with aiohttp.ClientSession() as session:
+        for i in range(0, num_of_objs):
+            body = measurements[i]
+            async with session.post(measurements_res, json=body, headers=headers, ssl=False) as resp:
+                if resp.status == 201:
+                    created_mes = await resp.json()
+    end = time.time()
+    dt = end - start
+    print(f"Elapsed time in Non-Bulk REST API with EdgeDB on creation of {num_of_objs} items is {dt} secs.")
+
+
+async def run_bulk_api_with_edgedb(num_of_objs: int):
+    measurements_res = "http://127.0.0.1:8059/api/MeasurementSeries"
+    measurements = list()
+    for i in range(0, num_of_objs):
+        measurements.append({
+            "sampleDate": str(datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S")),
+            "value": random.random() * 25,
+            "measureUnitId": "604a0412-ea47-11ed-b9a1-bba352c6e579",
+            "sensorId": "30ac8366-ea4b-11ed-ab17-e38db76a95f0"
+        })
+
+    headers = {
+        'Content-type': 'application/json',
+        'Accept': 'application/json'
+    }
+
+    start = time.time()
+    async with aiohttp.ClientSession() as session:
+        body = measurements
+        async with session.post(measurements_res, json=body, headers=headers, ssl=False) as resp:
+            if resp.status == 201:
+                created_mes = await resp.json()
+    end = time.time()
+    dt = end - start
+    print(f"Elapsed time in Bulk REST API with EdgeDB on creation of {num_of_objs} items is {dt} secs.")
+
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
